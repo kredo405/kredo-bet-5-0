5a8dbeff546c0f7869af80e6207829aaf12e381f
Kredo <kredik305@gmail.com>
2022-10-04 02:56:47 +0300

    create table outcomes percent

--- src/components/PoisonDestribution/PoisonDisrebution.jsx
+++ src/components/PoisonDestribution/PoisonDisrebution.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
-import { Avatar, List } from 'antd';
+import { Avatar, Progress } from 'antd';
 import axios from 'axios';
 
 const determineComposition = (dataHome, dataAway) => {
@@ -10,8 +10,8 @@ const determineComposition = (dataHome, dataAway) => {
 
     while (playerHomeFilter.length < 11) {
         dataHome.forEach((el, i, arr) => {
-            let maxGamesPlayersHome = Math.max(...arr.map(obj => +obj.games));
-            if (maxGamesPlayersHome === +el.games && playerHomeFilter.length < 11) {
+            let maxGamesPlayersHome = Math.max(...arr.map(obj => +obj.games_starts));
+            if (maxGamesPlayersHome === +el.games_starts && playerHomeFilter.length < 11) {
                 arr.splice(i, 1);
                 playerHomeFilter.push(el);
             }
@@ -20,16 +20,136 @@ const determineComposition = (dataHome, dataAway) => {
 
     while (playerAwayFilter.length < 11) {
         dataAway.forEach((el, i, arr) => {
-            let maxGamesPlayerAway = Math.max(...arr.map(obj => +obj.games));
-            if (maxGamesPlayerAway === +el.games && playerAwayFilter.length < 11) {
+            let maxGamesPlayerAway = Math.max(...arr.map(obj => +obj.games_starts));
+            if (maxGamesPlayerAway === +el.games_starts && playerAwayFilter.length < 11) {
                 arr.splice(i, 1);
                 playerAwayFilter.push(el);
             }
         })
     }
 
-    console.log(playerHomeFilter)
-    console.log(playerAwayFilter)
+    return {
+        compositionHome: playerHomeFilter,
+        compositionAway: playerAwayFilter,
+    }
+}
+
+const calcIndividualTotal = (composition, shootingOpponentHome, shootingOpponentAway, shootingStatsHome,
+    shootingStatsAway) => {
+    let shotsHome = 0;
+    let shotsAway = 0;
+    let shotsHomeWithOpponent = 0;
+    let shotsAwayWithOpponent = 0;
+    let goalsHome = 0;
+    let goalsAway = 0;
+
+    composition.compositionHome.forEach(el => {
+        if (el.goals_per_shot !== '' && el.shots_total_per90 !== '') {
+            shotsHome += +el.shots_total_per90;
+        }
+    })
+
+    composition.compositionAway.forEach(el => {
+        if (el.goals_per_shot !== '' && el.shots_total_per90 !== '') {
+            shotsAway += +el.shots_total_per90;
+        }
+    })
+
+    shotsHomeWithOpponent = (+shootingOpponentAway[0].shots_total_per90 + shotsHome) / 2
+    shotsAwayWithOpponent = (+shootingOpponentHome[0].shots_total_per90 + shotsAway) / 2
+
+    goalsHome = shotsHomeWithOpponent * +shootingStatsHome[0].goals_per_shot
+    goalsAway = shotsAwayWithOpponent * +shootingStatsAway[0].goals_per_shot
+
+    console.log(goalsHome)
+    console.log(goalsAway)
+
+    return {
+        goalsHome: goalsHome,
+        goalsAway: goalsAway,
+    }
+}
+
+const calcPoisonDestribution = (golals) => {
+    // рассчиываем распределение паусона все матчи
+    function poissonGoals(expectedGoals, goals, number) {
+        return (
+            ((expectedGoals ** goals * 2.71828 ** -expectedGoals) / number) * 100
+        );
+    }
+    let poisonGoals0Home = poissonGoals(golals.goalsHome, 0, 1);
+    let poisonGoals0Away = poissonGoals(golals.goalsAway, 0, 1);
+    let poisonGoals1Home = poissonGoals(golals.goalsHome, 1, 1);
+    let poisonGoals1Away = poissonGoals(golals.goalsAway, 1, 1);
+    let poisonGoals2Home = poissonGoals(golals.goalsHome, 2, 2);
+    let poisonGoals2Away = poissonGoals(golals.goalsAway, 2, 2);
+    let poisonGoals3Home = poissonGoals(golals.goalsHome, 3, 6);
+    let poisonGoals3Away = poissonGoals(golals.goalsAway, 3, 6);
+    let poisonGoals4Home = poissonGoals(golals.goalsHome, 4, 24);
+    let poisonGoals4Away = poissonGoals(golals.goalsAway, 4, 24);
+    let poisonGoals5Home = poissonGoals(golals.goalsHome, 5, 120);
+    let poisonGoals5Away = poissonGoals(golals.goalsAway, 5, 120);
+
+    // рассчитываем вероятности прохода ставки по распределению паусона
+    const percentOutcomes = {
+        p1: (poisonGoals1Home * poisonGoals0Away) / 100 +
+            (poisonGoals2Home * poisonGoals0Away) / 100 +
+            (poisonGoals3Home * poisonGoals0Away) / 100 +
+            (poisonGoals4Home * poisonGoals0Away) / 100 +
+            (poisonGoals5Home * poisonGoals0Away) / 100 +
+            (poisonGoals2Home * poisonGoals1Away) / 100 +
+            (poisonGoals3Home * poisonGoals1Away) / 100 +
+            (poisonGoals4Home * poisonGoals1Away) / 100 +
+            (poisonGoals5Home * poisonGoals1Away) / 100 +
+            (poisonGoals3Home * poisonGoals2Away) / 100 +
+            (poisonGoals4Home * poisonGoals2Away) / 100 +
+            (poisonGoals5Home * poisonGoals2Away) / 100 +
+            (poisonGoals4Home * poisonGoals3Away) / 100 +
+            (poisonGoals5Home * poisonGoals3Away) / 100 +
+            (poisonGoals5Home * poisonGoals4Away) / 100,
+        p2: (poisonGoals0Home * poisonGoals1Away) / 100 +
+            (poisonGoals0Home * poisonGoals2Away) / 100 +
+            (poisonGoals0Home * poisonGoals3Away) / 100 +
+            (poisonGoals0Home * poisonGoals4Away) / 100 +
+            (poisonGoals0Home * poisonGoals5Away) / 100 +
+            (poisonGoals1Home * poisonGoals2Away) / 100 +
+            (poisonGoals1Home * poisonGoals3Away) / 100 +
+            (poisonGoals1Home * poisonGoals4Away) / 100 +
+            (poisonGoals1Home * poisonGoals5Away) / 100 +
+            (poisonGoals2Home * poisonGoals3Away) / 100 +
+            (poisonGoals2Home * poisonGoals4Away) / 100 +
+            (poisonGoals2Home * poisonGoals5Away) / 100 +
+            (poisonGoals3Home * poisonGoals4Away) / 100 +
+            (poisonGoals3Home * poisonGoals5Away) / 100 +
+            (poisonGoals4Home * poisonGoals5Away) / 100,
+        tu25: (poisonGoals0Home * poisonGoals0Away) / 100 +
+            (poisonGoals1Home * poisonGoals0Away) / 100 +
+            (poisonGoals0Home * poisonGoals1Away) / 100 +
+            (poisonGoals1Home * poisonGoals1Away) / 100 +
+            (poisonGoals2Home * poisonGoals0Away) / 100 +
+            (poisonGoals0Home * poisonGoals2Away) / 100,
+        to25: 100 - ((poisonGoals0Home * poisonGoals0Away) / 100 +
+            (poisonGoals1Home * poisonGoals0Away) / 100 +
+            (poisonGoals0Home * poisonGoals1Away) / 100 +
+            (poisonGoals1Home * poisonGoals1Away) / 100 +
+            (poisonGoals2Home * poisonGoals0Away) / 100 +
+            (poisonGoals0Home * poisonGoals2Away) / 100),
+        bts: 100 - ((poisonGoals0Home * poisonGoals0Away) / 100 +
+            (poisonGoals1Home * poisonGoals0Away) / 100 +
+            (poisonGoals2Home * poisonGoals0Away) / 100 +
+            (poisonGoals3Home * poisonGoals0Away) / 100 +
+            (poisonGoals4Home * poisonGoals0Away) / 100 +
+            (poisonGoals5Home * poisonGoals0Away) / 100 +
+            (poisonGoals0Home * poisonGoals1Away) / 100 +
+            (poisonGoals0Home * poisonGoals2Away) / 100 +
+            (poisonGoals0Home * poisonGoals3Away) / 100 +
+            (poisonGoals0Home * poisonGoals4Away) / 100 +
+            (poisonGoals0Home * poisonGoals5Away) / 100),
+        it1O1: poisonGoals2Home + poisonGoals3Home + poisonGoals4Home + poisonGoals5Home,
+        it2O1: poisonGoals2Away + poisonGoals3Away + poisonGoals4Away + poisonGoals5Away
+    }
+
+    return percentOutcomes;
 }
 
 const getPlayerStats = (standartStats, shootingStats) => {
@@ -58,41 +178,16 @@ const getPlayerStats = (standartStats, shootingStats) => {
     return playerStats;
 }
 
-
 const PoisonDestribution = (props) => {
     const state = useSelector((state) => state);
-    const [dataHome, setDataHome] = useState([{
-        assists:"0",
-        games:"5",
-        games_starts:"4",
-        goals:"0",
-        goals_per90:"0.00",
-        minutes:"351",
-        player:"Daniel Amartey",
-        position:"DF",
-        xg:"0.0",
-        xg_per90:"0.00",
-        goals_per_shot:"0.00",
-        goals_per_shot_on_target:"",
-        shots_on_target_per90:"0.00",
-        shots_total_per90:"0.26",
-    }]);
-    const [dataAway, setDataAway] = useState([{
-        assists:"0",
-        games:"5",
-        games_starts:"4",
-        goals:"0",
-        goals_per90:"0.00",
-        minutes:"351",
-        player:"Daniel Amartey",
-        position:"DF",
-        xg:"0.0",
-        xg_per90:"0.00",
-        goals_per_shot:"0.00",
-        goals_per_shot_on_target:"",
-        shots_on_target_per90:"0.00",
-        shots_total_per90:"0.26",
-    }]);
+    const [dataHome, setDataHome] = useState([{ player: "" }]);
+    const [dataAway, setDataAway] = useState([{ player: "" }]);
+    const [shootingOpponentHome, setShootingOpponentHome] = useState([]);
+    const [shootingOpponentAway, setShootingOpponentAway] = useState([]);
+    const [shootingStatsHome, setShootingStatsHome] = useState([]);
+    const [shootingStatsAway, setShootingStatsAway] = useState([]);
+    const [poisonTable, setPoisonTable] = useState('');
+    const [result, setResult] = useState(false);
 
     useEffect(() => {
         const options = {
@@ -104,23 +199,46 @@ const PoisonDestribution = (props) => {
             console.log(response.data);
             response.data.stats.forEach(el => {
                 if (el.leagueId === props.id) {
+
                     const standartStatsHome = el.standartStatsPlayers.filter(item => {
-                        if (props.homeName === item.team) {
+                        if (props.homeName.replace("-", " ") === item.team.replace("-", " ")) {
                             return item;
                         }
                     })
                     const standartStatsAway = el.standartStatsPlayers.filter(item => {
-                        if (props.awayName === item.team) {
+                        if (props.awayName.replace("-", " ") === item.team.replace("-", " ")) {
                             return item;
                         }
                     });
                     const shootingStatsHome = el.shootingStatsPlayers.filter(item => {
-                        if (props.homeName === item.team) {
+                        if (props.homeName.replace("-", " ") === item.team.replace("-", " ")) {
                             return item;
                         }
                     })
                     const shootingStatsAway = el.shootingStatsPlayers.filter(item => {
-                        if (props.awayName === item.team) {
+                        if (props.awayName.replace("-", " ") === item.team.replace("-", " ")) {
+                            return item;
+                        }
+                    });
+
+                    const shootingStatsHomeOpponent = el.shootingStatsOpponent.filter(item => {
+                        if (props.homeName.replace("-", " ") === item.team.replace("-", " ")) {
+                            return item;
+                        }
+                    })
+                    const shootingStatsAwayOpponent = el.shootingStatsOpponent.filter(item => {
+                        if (props.awayName.replace("-", " ") === item.team.replace("-", " ")) {
+                            return item;
+                        }
+                    });
+
+                    const shootingHome = el.shootingStats.filter(item => {
+                        if (props.homeName.replace("-", " ") === item.team.replace("-", " ")) {
+                            return item;
+                        }
+                    })
+                    const shootingAway = el.shootingStats.filter(item => {
+                        if (props.awayName.replace("-", " ") === item.team.replace("-", " ")) {
                             return item;
                         }
                     });
@@ -130,7 +248,10 @@ const PoisonDestribution = (props) => {
 
                     setDataHome(playerStatsHome);
                     setDataAway(playerStatsAway);
-
+                    setShootingOpponentHome(shootingStatsHomeOpponent);
+                    setShootingOpponentAway(shootingStatsAwayOpponent);
+                    setShootingStatsHome(shootingHome);
+                    setShootingStatsAway(shootingAway);
                 }
             });
 
@@ -150,9 +271,61 @@ const PoisonDestribution = (props) => {
     }
 
     const calcPoison = () => {
-        if(dataHome.length >= 11 && dataHome.length >= 11) {
-            determineComposition(dataHome, dataAway);
+
+        let compositions;
+
+        if (dataHome.length >= 11 && dataAway.length >= 11) {
+            compositions = determineComposition(dataHome, dataAway);
         }
+
+        const individualTotals = calcIndividualTotal(compositions, shootingOpponentHome, shootingOpponentAway, shootingStatsHome,
+            shootingStatsAway)
+
+        const percentOutcomes = calcPoisonDestribution(individualTotals)
+
+        const result =
+            <div>
+                <div className='flex border-b-2 border-solid justify-between items-center py-3'>
+                    <h5 className='text-cyan-800 text-2xl w-6/12'>Победа 1</h5>
+                    <Progress type="circle" percent={percentOutcomes.p1.toFixed(0)} />
+                    <p className='text-rose-600 text-2xl'>{(100 / percentOutcomes.p1).toFixed(2)}</p>
+                </div>
+                <div className='flex border-b-2 border-solid justify-between items-center py-3'>
+                    <h5 className='text-cyan-800 text-2xl w-6/12'>Победа 2</h5>
+                    <Progress type="circle" percent={percentOutcomes.p2.toFixed(0)} />
+                    <p className='text-rose-600 text-2xl'>{(100 / percentOutcomes.p2).toFixed(2)}</p>
+                </div>
+                <div className='flex border-b-2 border-solid justify-between items-center py-3'>
+                    <h5 className='text-cyan-800 text-2xl w-6/12'>Тотал больше 2.5</h5>
+                    <Progress type="circle" percent={percentOutcomes.to25.toFixed(0)} />
+                    <p className='text-rose-600 text-2xl'>{(100 / percentOutcomes.to25).toFixed(2)}</p>
+                </div>
+                <div className='flex border-b-2 border-solid justify-between items-center py-3'>
+                    <h5 className='text-cyan-800 text-2xl w-6/12'>Тотал меньше 2.5</h5>
+                    <Progress type="circle" percent={percentOutcomes.tu25.toFixed(0)} />
+                    <p className='text-rose-600 text-2xl'>{(100 / percentOutcomes.tu25).toFixed(2)}</p>
+                </div>
+                <div className='flex border-b-2 border-solid justify-between items-center py-3'>
+                    <h5 className='text-cyan-800 text-2xl w-6/12'>Обе забьют</h5>
+                    <Progress type="circle" percent={percentOutcomes.bts.toFixed(0)} />
+                    <p className='text-rose-600 text-2xl'>{(100 / percentOutcomes.bts).toFixed(2)}</p>
+                </div>
+                <div className='flex border-b-2 border-solid justify-between items-center py-3'>
+                    <h5 className='text-cyan-800 text-2xl w-6/12'>ИТ1 больше 1.5</h5>
+                    <Progress type="circle" percent={percentOutcomes.it1O1.toFixed(0)} />
+                    <p className='text-rose-600 text-2xl'>{(100 / percentOutcomes.it1O1).toFixed(2)}</p>
+                </div>
+                <div className='flex border-b-2 border-solid justify-between items-center py-3'>
+                    <h5 className='text-cyan-800 text-2xl w-6/12'>ИТ2 больше 1.5</h5>
+                    <Progress type="circle" percent={percentOutcomes.it2O1.toFixed(0)} />
+                    <p className='text-rose-600 text-2xl'>{(100 / percentOutcomes.it2O1).toFixed(2)}</p>
+                </div>
+            </div>
+
+        setPoisonTable(result)
+        setResult(true)
+
+        console.log(percentOutcomes)
     }
 
     const listHome = dataHome.map((el, i) => {
@@ -179,30 +352,36 @@ const PoisonDestribution = (props) => {
         )
     })
 
-
-
     return (
         <div className='container'>
-            <div className="flex justify-center py-2">
-                <h2 className='text-xl'>Выберите травмированных игроков {props.homeName}</h2>
-            </div>
-            <ul>
-                {listHome}
-            </ul>
-            <div className="flex justify-center py-2">
-                <h2 className='text-xl'>Выберите травмированных игроков {props.awayName}</h2>
-            </div>
-            <ul>
-                {listAway}
-            </ul>   
-            <div className="flex justify-center py-2 mb-5 ">
-                <button
-                    className='text-xl bg-cyan-600 text-white px-10 py-3 rounded-lg border-2 border-cyan-600 border-solid'
-                    onClick={calcPoison}
-                >
-                    Рассчитать
-                </button>
-            </div>
+            {result ?
+                <>
+                    {poisonTable}
+                </>
+                :
+                <>
+                    <div className="flex justify-center py-2">
+                        <h2 className='text-xl'>Выберите травмированных игроков {props.homeName}</h2>
+                    </div>
+                    <ul>
+                        {listHome}
+                    </ul>
+                    <div className="flex justify-center py-2">
+                        <h2 className='text-xl'>Выберите травмированных игроков {props.awayName}</h2>
+                    </div>
+                    <ul>
+                        {listAway}
+                    </ul>
+                    <div className="flex justify-center py-2 mb-5 ">
+                        <button
+                            className='text-xl bg-cyan-600 text-white px-10 py-3 rounded-lg border-2 border-cyan-600 border-solid'
+                            onClick={calcPoison}
+                        >
+                            Рассчитать
+                        </button>
+                    </div>
+                </>
+            }
         </div>
     )
 }
